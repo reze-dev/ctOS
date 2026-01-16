e42b6da8c16524c0eab58051d4f547858ac19712
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-01-16 11:58:30 +0530

    fix: remove kitty window decorations

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -23,7 +23,7 @@
     extraConfig = ''
       font_family family='VictorMono Nerd Font' style=SemiBold
       #text_fg_override_threshold 4.5 ratio
-      hide_window_decoration yes
+      hide_window_decorations yes
     '';
   };
 }
