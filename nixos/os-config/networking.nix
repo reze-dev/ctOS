ac49988f458e3933f0c6a0550d98b0ebff0a1df9
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-03-30 19:40:28 +0530

    add ocp hosts

--- nixos/os-config/networking.nix
+++ nixos/os-config/networking.nix
@@ -12,6 +12,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   networking.extraHosts = ''
+    34.136.150.68 console-openshift-console.apps.ocp.ocp-lab.satyabrata.net oauth-openshift.apps.ocp.ocp-lab.satyabrata.net
     # 192.168.122.216 gitlab.internal
     # 192.168.122.56 jenkins.internal
   '';
