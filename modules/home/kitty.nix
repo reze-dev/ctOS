c0a41d2e83b5df7536de585485449074b8873084
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-04-05 00:51:16 +0530

    refactor: move home/apps → modules/home/{ghostty,kitty}.nix with snowflake.home.* options

---
+++ modules/home/kitty.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+let cfg = config.snowflake.home.kitty;
+in {
+  options.snowflake.home.kitty.enable = lib.mkEnableOption "Kitty terminal";
+
+  config = lib.mkIf cfg.enable {
+    programs.kitty = {
+      enable = true;
+      font.name = "Victor Mono Nerd Font";
+      font.size = 13;
+      shellIntegration = {
+        enableFishIntegration = true;
+        enableZshIntegration = true;
+      };
+      themeFile = "rose-pine";
+      settings = {
+        background_opacity = 0.85;
+        confirm_os_window_close = 0;
+        shell = "fish";
+      };
+      extraConfig = ''
+        font_family family='VictorMono Nerd Font' style=SemiBold
+        hide_window_decorations yes
+      '';
+    };
+  };
+}
