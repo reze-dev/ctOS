7eb13b4f568c77aecc506ed2293dc53a3f9bcb19
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-04-05 02:29:14 +0530

    go-installer.nix: add runtime deps wrapper and fix build
    - Wrap binary with makeWrapper to inject git, lsblk, parted,
      nixos-install-tools, etc. onto PATH at runtime
    - Remove stale install.py copy from embedded source
    - Add go.sum to source assembly
    - Set vendorHash for golang.org/x/term dependency

--- parts/go-installer.nix
+++ parts/go-installer.nix
@@ -9,11 +9,11 @@
         mkdir -p $out/flake
         cp ${../installer}/*.go $out/
         cp ${../installer}/go.mod $out/
+        cp ${../installer}/go.sum $out/
 
         # Copy flake files for embedding
         cp ${../flake.nix} $out/flake/
         cp ${../flake.lock} $out/flake/
-        cp ${../install.py} $out/flake/
         cp ${../README.md} $out/flake/
         cp -r ${../hosts} $out/flake/hosts
         cp -r ${../home} $out/flake/home
@@ -21,14 +21,37 @@
         cp -r ${../parts} $out/flake/parts
         cp -r ${../assets} $out/flake/assets
       '';
-    in
-    {
-      packages.go-installer = pkgs.buildGoModule {
-        pname = "snowflake-installer";
+
+      # Runtime tools the installer shells out to
+      runtimeDeps = with pkgs; [
+        git
+        coreutils
+        util-linux     # lsblk, blkid, mkswap, swapon, mountpoint
+        pciutils       # lspci (GPU detection)
+        whois          # mkpasswd
+        openssl        # fallback password hashing
+        parted         # partition creation (dual-boot mode)
+        btrfs-progs    # mkfs.btrfs, btrfs subvolume
+        e2fsprogs      # chattr
+        nixos-install-tools
+      ];
+
+      goBin = pkgs.buildGoModule {
+        pname = "snowflake-installer-unwrapped";
         version = "1.0.0";
         src = goSrc;
-        vendorHash = null;
-
+        vendorHash = "sha256-GlwDnvLCTNLID0SW1wHh4f5O1t0sz+NX/k4RdC1jKk0=";
+      };
+    in
+    {
+      packages.go-installer = pkgs.symlinkJoin {
+        name = "snowflake-installer";
+        paths = [ goBin ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/snowflake-installer \
+            --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+        '';
         meta = {
           description = "Snowflake NixOS installer (Go binary with embedded flake)";
           mainProgram = "snowflake-installer";
@@ -42,3 +65,4 @@
       };
     };
 }
+
