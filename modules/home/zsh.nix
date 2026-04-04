529a980b93da168cc913358721e759017d027fac
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-04-05 00:51:35 +0530

    refactor: move home/shell-config → modules/home/{fish,zsh}.nix with snowflake.home.* options

---
+++ modules/home/zsh.nix
@@ -0,0 +1,70 @@
+{ config, lib, pkgs, ... }:
+let cfg = config.snowflake.home.zsh;
+in {
+  options.snowflake.home.zsh.enable = lib.mkEnableOption "Zsh shell configuration";
+
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      antidote = {
+        enable = true;
+        plugins = [
+          "Aloxaf/fzf-tab"
+          "zsh-users/zsh-syntax-highlighting"
+        ];
+        useFriendlyNames = true;
+      };
+      initContent = lib.mkMerge [
+        (lib.mkOrder 550 ''
+          EDITOR=nvim
+          export GPG_TTY=$(tty)
+          export PATH=$HOME/.config/emacs/bin:$PATH
+          export PATH=$(go env GOPATH)/bin:$PATH
+          export DIRENV_LOG_FORMAT=""
+        '')
+        (lib.mkOrder 1000 ''
+          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+          zstyle ':completion:*' menu no
+          zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --icons $realpath'
+          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --icons $realpath'
+          eval "$(fzf --zsh)"
+          eval "$(zoxide init --cmd cd zsh)"
+          eval "$(direnv hook zsh)"
+        '')
+      ];
+      completionInit = ''
+        autoload -U compinit && compinit
+      '';
+      autosuggestion.enable = true;
+      defaultKeymap = "emacs";
+      enableCompletion = true;
+      history = {
+        append = true;
+        expireDuplicatesFirst = true;
+        extended = true;
+        ignoreAllDups = true;
+        ignoreDups = true;
+        ignoreSpace = true;
+        save = 5000;
+        share = true;
+        size = 5000;
+        path = "$HOME/.histfile";
+      };
+      sessionVariables = {
+        COLORTERM = "24bit";
+        TERM = "xterm-256color";
+      };
+      shellAliases = {
+        cat = "bat";
+        ll = "eza -l --icons --no-permissions";
+        ls = "eza --icons";
+        tree = "eza -T --icons";
+        la = "eza -la --icons";
+        lo = "eza -l -o --icons";
+        vim = "nvim";
+        butt = "but";
+      };
+    };
+  };
+}
