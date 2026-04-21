9776d4f242e04fda42697cb75d6cb48109c23fe3
atomiksan <25588579+atomiksan@users.noreply.github.com>
2026-04-21 16:26:21 +0530

    change github config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,14 +1,16 @@
 { config, lib, ... }:
-let cfg = config.northstar.home.git;
-in {
+let
+  cfg = config.northstar.home.git;
+in
+{
   options.northstar.home.git.enable = lib.mkEnableOption "Git user configuration";
 
   config = lib.mkIf cfg.enable {
     programs.git = {
       enable = true;
       settings = {
-        user.name = "atomiksan";
-        user.email = "25588579+atomiksan@users.noreply.github.com";
+        user.name = "reze-dev";
+        user.email = "25588579+reze-dev@users.noreply.github.com ";
         init.defaultBranch = "main";
       };
     };
