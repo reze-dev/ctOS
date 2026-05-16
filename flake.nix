863c4feceef7a6b817c39d3c3cd5fa79dd83d8d1
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-05-16 15:09:48 +0530

    refactor: add hyprland cachix substitutor

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
     extra-substituters = [
       "https://cache.nixos.org/"
       "https://noctalia.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
