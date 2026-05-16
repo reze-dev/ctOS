e0883a28d70e085a74807622f35c452b551f9c05
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-05-16 14:51:01 +0530

    fix: remove duplicate zen browser

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 let
@@ -14,51 +13,46 @@ in
   config = lib.mkIf cfg.enable {
     nixpkgs.config.allowUnfree = true;
 
-    environment.systemPackages =
-      with pkgs;
-      [
-        bat
-        btop
-        codecrafters-cli
-        discord
-        emacs-pgtk
-        eza
-        fd
-        fzf
-        fuzzel
-        gcc
-        ghostty
-        go
-        hyprcursor
-        kitty
-        libgcc
-        libnotify
-        nil
-        nitch
-        nixfmt
-        nodejs
-        obsidian
-        redis
-        ripgrep
-        rustup
-        tmux
-        unzip
-        vscode
-        wget
-        wl-clipboard
-        yazi
-        zig
-        zoxide
-        jdk17
-        clang
-        clang-tools
-        cmake
-        gnumake
-        shfmt
-        shellcheck
-      ]
-      ++ [
-        inputs.zen-browser.packages."${stdenv.hostPlatform.system}".default
-      ];
+    environment.systemPackages = with pkgs; [
+      bat
+      btop
+      codecrafters-cli
+      discord
+      emacs-pgtk
+      eza
+      fd
+      fzf
+      fuzzel
+      gcc
+      ghostty
+      go
+      hyprcursor
+      kitty
+      libgcc
+      libnotify
+      nil
+      nitch
+      nixfmt
+      nodejs
+      obsidian
+      redis
+      ripgrep
+      rustup
+      tmux
+      unzip
+      vscode
+      wget
+      wl-clipboard
+      yazi
+      zig
+      zoxide
+      jdk17
+      clang
+      clang-tools
+      cmake
+      gnumake
+      shfmt
+      shellcheck
+    ];
   };
 }
