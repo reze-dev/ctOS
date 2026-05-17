7d643cf82574b4868d99837fc1f70665cc10fae1
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-05-17 15:41:45 +0530

    remove cosmic desktop and cosmic dm and add tuigreet

--- modules/nixos/display.nix
+++ modules/nixos/display.nix
@@ -1,13 +1,44 @@
-{ config, lib, pkgs, ... }:
-let cfg = config.northstar.display;
-in {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.northstar.display;
+  tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+  hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+in
+{
   options.northstar.display.enable = lib.mkEnableOption "display manager and desktop environment";
 
   config = lib.mkIf cfg.enable {
-    # COSMIC desktop + greeter
-    services.displayManager.cosmic-greeter.enable = true;
-    services.desktopManager.cosmic.enable = true;
 
+    # Tuigreet greeter
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
+          user = "greeter";
+        };
+      };
+    };
+
+    # this is a life saver.
+    # literally no documentation about this anywhere.
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
+
+    # Emable Niri
     programs.niri.enable = true;
 
     # Keymap
