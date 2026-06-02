99828be2ec2c7081d6ea256e088bde14ff6ee6b2
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-06-02 14:02:42 +0530

    flake: update flake inputs

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,11 +1,13 @@
 { config, lib, ... }:
-let cfg = config.northstar.networking;
-in {
+let
+  cfg = config.northstar.networking;
+in
+{
   options.northstar.networking.enable = lib.mkEnableOption "NetworkManager and host entries";
 
   config = lib.mkIf cfg.enable {
     networking.extraHosts = ''
-      34.57.215.58 console-openshift-console.apps.ocp.ocp-lab.satyabrata.net oauth-openshift.apps.ocp.ocp-lab.satyabrata.net
+      10.250.18.140 console-openshift-console.apps.nonprod.odisha.gov.in oauth-openshift.apps.nonprod.odisha.gov.in
     '';
 
     networking.networkmanager.enable = true;
