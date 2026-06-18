e4a011112528b0725053abdfab7001ff8d9233c5
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-06-18 11:09:41 +0530

    chore: change grub theme

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -24,7 +24,7 @@ in
 
         dedsec-theme = {
           enable = true;
-          style = "wannacry";
+          style = "lovetrap";
           icon = "color";
           resolution = "1080p";
         };
