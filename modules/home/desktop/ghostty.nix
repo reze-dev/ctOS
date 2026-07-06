97bc0f42d45774b0ac74c45a905cb98cc399b876
reze-dev <25588579+reze-dev@users.noreply.github.com >
2026-07-06 19:24:26 +0530

    chore(packages): add kde dolphin, remove ghostty

--- modules/home/desktop/ghostty.nix
+++ modules/home/desktop/ghostty.nix
@@ -12,7 +12,7 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.ghostty = {
-      enable = true;
+      enable = false;
       enableFishIntegration = true;
       enableZshIntegration = true;
       installBatSyntax = true;
