53d253e1b9c2907c82651e4d53914b8e564704b5
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-07-19 00:44:25 +0530

    nixos/development: rename dev.nix and emacs.nix

---
+++ modules/nixos/development/emacs.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.northstar.emacs;
+in
+{
+  options.northstar.emacs.enable = lib.mkEnableOption "Emacs daemon service";
+
+  config = lib.mkIf cfg.enable {
+    services.emacs = {
+      enable = true;
+      package = pkgs.emacs-pgtk;
+    };
+  };
+}
