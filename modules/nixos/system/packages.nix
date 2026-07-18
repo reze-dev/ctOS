2703c1de152b2c467aae8bbb16f4360fc7780f9f
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-07-19 00:44:25 +0530

    nixos/system: configure system packages, udisks2 and gvfs

---
+++ modules/nixos/system/packages.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.northstar.packages;
+in
+{
+  options.northstar.packages.enable = lib.mkEnableOption "system packages and unfree config";
+
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = true;
+
+    services.udisks2.enable = true;
+    services.gvfs.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      alacritty
+      bat
+      btop
+      eza
+      fd
+      fzf
+      fuzzel
+      hyprcursor
+      jq
+      kdePackages.dolphin
+      kitty
+      libnotify
+      mpv
+      nitch
+      obsidian
+      openconnect
+      ripgrep
+      tmux
+      unzip
+      wget
+      wl-clipboard
+      zoxide
+    ];
+  };
+}
