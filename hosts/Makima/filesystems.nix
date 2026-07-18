9e5f7097a1a41a4175bc4a7340596ebd326445c0
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-07-19 04:36:35 +0530

    chore: fix formatting

--- hosts/Makima/filesystems.nix
+++ hosts/Makima/filesystems.nix
@@ -3,31 +3,47 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/5e97ec6f-410e-46e1-a228-ee129541985b";
     fsType = "btrfs";
-    options = [ "subvol=@root" "compress=zstd" ];
+    options = [
+      "subvol=@root"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/home" = {
     device = "/dev/disk/by-uuid/5e97ec6f-410e-46e1-a228-ee129541985b";
     fsType = "btrfs";
-    options = [ "subvol=@home" "compress=zstd" ];
+    options = [
+      "subvol=@home"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/nix" = {
     device = "/dev/disk/by-uuid/5e97ec6f-410e-46e1-a228-ee129541985b";
     fsType = "btrfs";
-    options = [ "subvol=@nix" "compress=zstd" "noatime" ];
+    options = [
+      "subvol=@nix"
+      "compress=zstd"
+      "noatime"
+    ];
   };
 
   fileSystems."/var/log" = {
     device = "/dev/disk/by-uuid/5e97ec6f-410e-46e1-a228-ee129541985b";
     fsType = "btrfs";
-    options = [ "subvol=@log" "compress=zstd" ];
+    options = [
+      "subvol=@log"
+      "compress=zstd"
+    ];
   };
 
   fileSystems."/boot/efi" = {
     device = "/dev/disk/by-uuid/238D-76D7";
     fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
   };
 
   fileSystems."/swap" = {
@@ -36,7 +52,5 @@
     options = [ "subvol=@swap" ];
   };
 
-  swapDevices = [
-    { device = "/swap/swapfile"; }
-  ];
+  swapDevices = [ { device = "/swap/swapfile"; } ];
 }
