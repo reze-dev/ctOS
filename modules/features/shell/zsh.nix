66502e3cc5a6c0bbc45f7c953a9c626f892eab5e
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-07-26 02:26:25 +0530

    fix: fzf errors in zshrc

--- modules/features/shell/zsh.nix
+++ modules/features/shell/zsh.nix
@@ -41,8 +41,8 @@ in
                   zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
                   zstyle ':completion:*' menu no
                   zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-                  zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --icons $realpath'
-                  zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --icons $realpath'
+                  zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --icons=always $realpath'
+                  zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --icons=always $realpath'
                   eval "$(fzf --zsh)"
                   eval "$(zoxide init --cmd cd zsh)"
                 '')
