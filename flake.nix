884bd2ada9d29181917cd9095dc2678968507ab4
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-07-31 01:21:58 +0530

    fix: use correct cachix public key for niri

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "niri.cachix.org-1:Wl0FBSlusqxy5PWyOOKtOaJTXTu8BHfciQDnAznVKmA="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
