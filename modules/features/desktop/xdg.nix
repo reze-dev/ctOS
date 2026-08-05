c8f9dc6debc8451ab0d455516e61b033b6108680
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-08-05 18:42:13 +0530

    fix(desktop): fix zen browser portal and dolphin mime apps handling
    - fix zen desktop file name in mime associations
    - set GTK_USE_PORTAL=1 for zen/gtk file chooser
    - create applications.menu to fix dolphin KIO application cache on non-plasma wms

--- modules/features/desktop/xdg.nix
+++ modules/features/desktop/xdg.nix
@@ -20,27 +20,27 @@ let
         xdg.mimeApps = {
           enable = true;
           associations.added = {
-            "text/html" = [ "zen.desktop" ];
-            "x-scheme-handler/http" = [ "zen.desktop" ];
-            "x-scheme-handler/https" = [ "zen.desktop" ];
+            "text/html" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/https" = [ "zen-beta.desktop" ];
           };
           defaultApplications = {
             # File manager / Directories
             "inode/directory" = [ "org.kde.dolphin.desktop" ];
 
             # Web / URLs
-            "text/html" = [ "zen.desktop" ];
-            "x-scheme-handler/http" = [ "zen.desktop" ];
-            "x-scheme-handler/https" = [ "zen.desktop" ];
-            "x-scheme-handler/about" = [ "zen.desktop" ];
-            "x-scheme-handler/unknown" = [ "zen.desktop" ];
+            "text/html" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/about" = [ "zen-beta.desktop" ];
+            "x-scheme-handler/unknown" = [ "zen-beta.desktop" ];
 
             # Images (qView primary)
             "image/png" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "org.kde.dolphin.desktop" ];
             "image/jpeg" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "org.kde.dolphin.desktop" ];
             "image/webp" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "org.kde.dolphin.desktop" ];
             "image/gif" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "org.kde.dolphin.desktop" ];
-            "image/svg+xml" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "zen.desktop" ];
+            "image/svg+xml" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" "zen-beta.desktop" ];
             "image/bmp" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
             "image/tiff" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
             "image/avif" = [ "com.interversehq.qView.desktop" "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
@@ -58,7 +58,7 @@ let
             "audio/ogg" = [ "mpv.desktop" ];
 
             # PDF & Documents (Okular primary, Zathura terminal fallback, Zen)
-            "application/pdf" = [ "org.kde.okular.desktop" "org.pwmt.zathura.desktop" "zen.desktop" ];
+            "application/pdf" = [ "org.kde.okular.desktop" "org.pwmt.zathura.desktop" "zen-beta.desktop" ];
             "application/epub+zip" = [ "org.kde.okular.desktop" "org.pwmt.zathura.desktop" ];
             "application/postscript" = [ "org.kde.okular.desktop" "org.pwmt.zathura.desktop" ];
 
@@ -80,6 +80,17 @@ let
             "application/xml" = [ "dev.zed.Zed.desktop" "ghostty.desktop" ];
           };
         };
+
+        xdg.configFile."menus/applications.menu".text = ''
+          <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
+           "http://www.freedesktop.org/standards/menu-spec/1.0/menu.dtd">
+          <Menu>
+            <Name>Applications</Name>
+            <DefaultAppDirs/>
+            <DefaultDirectoryDirs/>
+            <DefaultMergeDirs/>
+          </Menu>
+        '';
       };
     };
 in
@@ -120,5 +131,9 @@ in
     };
 
     home-manager.sharedModules = [ hmXdgModule ];
+
+    environment.sessionVariables = {
+      GTK_USE_PORTAL = "1";
+    };
   };
 }
