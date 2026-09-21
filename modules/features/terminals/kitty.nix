8807eb9ddb7fad8e85b71a2cc37d93a9f056369a
reze-dev <25588579+reze-dev@users.noreply.github.com>
2026-09-21 19:11:16 +0530

    chore(kitty): update font and font features

--- modules/features/terminals/kitty.nix
+++ modules/features/terminals/kitty.nix
@@ -34,9 +34,9 @@ in
               extraConfig = ''
                 include ${../../../shell/config/kitty/themes/monoglow.conf}
 
-                font_family family='MonoLisa' style=Regular features="+calt +liga +salt"
+                font_family family='MonoLisa' style=Regular features="+calt +liga +salt +ss07 +ss08"
                 bold_font        auto
-                italic_font family='MonoLisa Italic' style=Regular features="+calt +liga +salt +ss02"
+                italic_font family='MonoLisa Italic' style=Regular features="+calt +liga +salt +ss02 +ss07 +ss08"
                 bold_italic_font auto
                 window_padding_width 12
                 hide_window_decorations yes
